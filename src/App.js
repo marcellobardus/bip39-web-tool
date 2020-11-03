0dc3db0120d0d80a5be6115c3903a6f22c73072e
Marcello Bardus <marcellobardus8@gmail.com>
2020-11-03 16:55:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "rsuite/lib/styles/index.less";
+
+import Navigation from "./components/Navigation";
+import Decrypt from "./views/Decrypt";
+import Encrypt from "./views/Encrypt";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/encrypt">
+          <Encrypt />
+        </Route>
+        <Route path="/">
+          <Decrypt />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
