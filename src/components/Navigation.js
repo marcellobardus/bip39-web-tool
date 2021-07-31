99715543973ac30d1675ba7559fdfae0d7f19a94
Aimeric Seguin <aimeric.seguin@gmail.com>
2021-07-31 20:35:50 +0200

    Fixed Nav.Item

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-import { Nav, Navbar, Icon, Sidenav } from "rsuite";
-
-const panelStyles = {
-  padding: "15px 20px",
-  color: "#aaa",
-};
+import { Nav, Icon, Sidenav } from "rsuite";
 
 export default function Navigation() {
   const [expanded, setExpanded] = useState(false);
+
   return (
     <div>
       <Icon
         style={{ alignSelf: "center" }}
-        icon="arrow-circle-o-right"
+        icon='arrow-circle-o-right'
         onClick={() => {
           setExpanded(!expanded);
         }}
@@ -28,23 +23,53 @@ export default function Navigation() {
       >
         <Sidenav.Body>
           <Nav>
-            <Nav.Item eventKey="1" icon={<Icon icon="eye" />}>
-              <Link to={"/aes-decrypt"}> AES-Decrypt </Link>
+            <Nav.Item
+              eventKey='1'
+              icon={<Icon icon='eye-slash' />}
+              componentClass={Link}
+              to={"/aes-encrypt"}
+            >
+              AES-ENCRYPT
             </Nav.Item>
-            <Nav.Item eventKey="2" icon={<Icon icon="eye-slash" />}>
-              <Link to={"/aes-encrypt"}> AES-Encrypt </Link>
+            <Nav.Item
+              eventKey='2'
+              icon={<Icon icon='eye-slash' />}
+              componentClass={Link}
+              to={"/aes-decrypt"}
+            >
+              AES-DECRYPT
             </Nav.Item>
-            <Nav.Item eventKey="3" icon={<Icon icon="eye-slash" />}>
-              <Link to={"/ecdsa-encrypt"}> ECDSA-Encrypt </Link>
+            <Nav.Item
+              eventKey='3'
+              icon={<Icon icon='eye-slash' />}
+              componentClass={Link}
+              to={"/ecdsa-encrypt"}
+            >
+              ECDSA-Encrypt
             </Nav.Item>
-            <Nav.Item eventKey="4" icon={<Icon icon="eye" />}>
-              <Link to={"/ecdsa-decrypt"}> ECDSA-Decrypt </Link>
+            <Nav.Item
+              eventKey='4'
+              icon={<Icon icon='eye-slash' />}
+              componentClass={Link}
+              to={"/ecdsa-decrypt"}
+            >
+              ECDSA-Decrypt
             </Nav.Item>
-            <Nav.Item eventKey="5" icon={<Icon icon="wrench" />}>
-              <Link to={"/derivate"}> Derivation </Link>
+            <Nav.Item
+              eventKey='5'
+              icon={<Icon icon='eye-slash' />}
+              componentClass={Link}
+              to={"/derivation"}
+            >
+              Derivation
             </Nav.Item>
-            <Nav.Item eventKey="6" icon={<Icon icon="book" />}>
-              <Link to={"/mnemonic"}> Mnemonic </Link>
+            <Nav.Item
+              eventKey='6'
+              icon={<Icon icon='eye-slash' />}
+              componentClass={Link}
+              to={"/mnemonic"}
+            >
+              Mnemonic
             </Nav.Item>
           </Nav>
         </Sidenav.Body>
